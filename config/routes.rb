4b3a95998e5e14ad3569f8ba9eb1408682eb3eb5
Chris Nelson <chris.nelson.mn@gmail.com>
2014-04-13 13:16:43 -0500

    Add heroku subdomains to routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Catarse::Application.routes.draw do
 #  mount CatarseWepay::Engine => "/", as: :catarse_wepay
 
   # Channels
-  constraints subdomain: /^(?!www|secure|test|local)(\w+)/ do
+  constraints subdomain: /^(?!www|secure|test|local|hoodstarter-demo|hoodstarter-prod)(\w+)/ do
     namespace :channels, path: '' do
       namespace :admin do
         namespace :reports do
