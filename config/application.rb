1a6193881bd5759ddbf5aed33d86ecb22cac1abf
Chris Nelson <chris.nelson.mn@gmail.com>
2014-04-13 13:50:56 -0500

    Set Central time zone

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Catarse
     config.encoding = "utf-8"
 
     config.filter_parameters += [:password, :password_confirmation]
-    config.time_zone = 'Brasilia'
+    config.time_zone = 'Central Time (US & Canada)'
     config.active_record.default_timezone = :local
     config.generators do |g|
       g.test_framework :rspec, fixture: false, views: false
